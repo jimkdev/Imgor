37cb9dcec91381f45ed87f5af71ecf12fdc93615
jimkdev <jimkdev150@gmail.com>
2026-01-04 22:37:42 +0200

    Create and load some styles

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os.path
+
 from PySide6.QtWidgets import QApplication
 
 from components import MainWindow
 
 if __name__ == "__main__":
     app = QApplication([])
+
+    with open(os.path.join(os.path.dirname(__file__), "styles", "index.qss"), "r") as f:
+        stylesheet = f.read()
+        app.setStyleSheet(stylesheet)
+
     window = MainWindow()
     window.show()
     app.exec()
