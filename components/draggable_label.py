704d59fd0be86cef3fac17d51e3e67384d12a148
jimkdev <jimkdev150@gmail.com>
2026-01-17 21:11:38 +0200

    Make draggable image to be inside the parent container

--- components/draggable_label.py
+++ components/draggable_label.py
@@ -22,6 +22,9 @@ class DraggableLabel(QLabel):
         if self._dragging:
             # Move relative to parent
             new_pos = self.mapToParent(event.position().toPoint() - self._drag_offset)
+            parent_rect = self.parent().rect()
+            new_pos.setX(max(0, min(new_pos.x(), parent_rect.width() - self.width())))
+            new_pos.setY(max(0, min(new_pos.y(), parent_rect.height() - self.height())))
             self.move(new_pos)
             event.accept()
         else:
